5912011979d13cbbba85838d81bdbf99d4f83ac8
julz-code <juliannetmurdock@gmail.com>
2024-11-20 10:22:05 -0500

    Fix Vercel deployment for Express app

--- index.js
+++ index.js
@@ -12,8 +12,5 @@ app.get('/', (req, res) => {
     res.render('index'); // This will render the 'views/index.ejs' file
 });
 
-// Set the port for the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// Export the app to be used by Vercel serverless functions
+module.exports = app;
